d608f6f72035dd18a637de539079d5d25b2f2dc2
davidacostaborges21-stack <davidacostaborges21@gmail.com>
2026-04-16 16:29:36 -0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-const current = location.pathname.split('/').pop() || 'index.html';
-document.querySelectorAll('[data-nav]').forEach(link => {
-  if (link.getAttribute('href') === current) link.classList.add('active');
-});
